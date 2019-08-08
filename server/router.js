65bcbed97046b5f2222c0e4c3d66a09573ece011
dposse <dposse.125@gmail.com>
2019-08-08 10:09:52 -0400

    Added get route with auth

--- server/router.js
+++ server/router.js
@@ -8,4 +8,7 @@ const requireSignin = passport.authenticate('local', { session: false })
 module.exports = function(app) {
   app.post('/auth/signin', requireSignin, Authentication.signin)
   app.post('/auth/signup', Authentication.signup)
+  app.get('/watchlist', requireAuth, (req, res) => {
+    res.send('cool');
+  });
 }
